6cd4296c7531face2cec6e65a00120c9a5f599d6
Vitor Gagliani <93485462+VitorGagliani@users.noreply.github.com>
2026-02-16 15:52:46 -0300

    @JsonIgnore

--- src/main/java/com/ecommerce/entity/ItemPedido.java
+++ src/main/java/com/ecommerce/entity/ItemPedido.java
@@ -2,6 +2,8 @@ package com.ecommerce.entity;
 
 import java.math.BigDecimal;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
@@ -29,6 +31,7 @@ public class ItemPedido {
 	
 	@ManyToOne(optional = false)
     @JoinColumn(name = "pedido_id")
+	@JsonIgnore
     private Pedido pedido;
 
     @ManyToOne(optional = false)
